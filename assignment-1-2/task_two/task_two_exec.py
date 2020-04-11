02ea9fe84bda64200d770bab26c95dffe869cdb6
Rim Rakhimov <rimrakhimov@gmail.com>
2020-04-11 16:40:09 +0300

    assignment folder renamed

---
+++ assignment-1-2/task_two/task_two_exec.py
@@ -0,0 +1,34 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class TaskTwo(object):
+
+    @staticmethod
+    def run(task):
+        log.info("Started SECOND task")
+
+        task_result = {}
+
+        try:
+            ## YOUR ALGORITHM HERE
+            print()
+            print("Task2: " + str(task))
+            print()
+            task_result["status"] = "Success"
+
+        except Exception as e:
+            log.error("Error running task: {0}".format(task))
+            task_result["status"] = "Failed"
+        else:
+            pass
+        return task_result
+
+    @staticmethod
+    def child_task_type(task):
+        return '\n\n***\n\n'
+
+    @staticmethod
+    def has_child_task(task):
+        return False
